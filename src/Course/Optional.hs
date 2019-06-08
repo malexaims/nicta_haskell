f35a78c87683a567f5395974093c589cbbdbd642
malexaims <amjunk2007@hotmail.com>
2019-06-07 20:02:02 -0400

    Optional complete

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -58,8 +58,8 @@ bindOptional f (Full x) = f x
   Optional a
   -> a
   -> a
-(??) =
-  error "todo: Course.Optional#(??)"
+(??) Empty x = x
+(??) (Full x) _ = x
 
 -- | Try the first optional for a value. If it has a value, use it; otherwise,
 -- use the second value.
@@ -79,8 +79,8 @@ bindOptional f (Full x) = f x
   Optional a
   -> Optional a
   -> Optional a
-(<+>) =
-  error "todo: Course.Optional#(<+>)"
+(<+>) Empty x = x
+(<+>) (Full x) _ = Full x
 
 -- | Replaces the Full and Empty constructors in an optional.
 --
@@ -94,8 +94,8 @@ optional ::
   -> b
   -> Optional a
   -> b
-optional =
-  error "todo: Course.Optional#optional"
+optional _ x Empty = x
+optional f _ (Full x) = f x
 
 applyOptional :: Optional (a -> b) -> Optional a -> Optional b
 applyOptional f a = bindOptional (\f' -> mapOptional f' a) f
